4185a2e8d7cd4d412ed464c23e18bb20ea48a107
Henrique B. Madeira <110936410+HenriqueHBM@users.noreply.github.com>
2022-11-25 23:35:21 -0300

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,25 @@
+ //pegando os valores digita nos input e passando para uma variavel
+ var n1 = document.querySelector("#n1");
+ var n2 = document.querySelector("#n2");
+ var resultado = document.querySelector("span");
+
+ function somar() {
+     //somando os valores digitados
+     resultado.innerHTML = parseInt(n1.value) + (parseInt(n2.value));
+ }
+
+ function subtrair() {
+     resultado.innerHTML = parseInt(n1.value) - (parseInt(n2.value));
+ }
+
+ function multiplicar() {
+     resultado.innerHTML = parseInt(n1.value) * (parseInt(n2.value));
+ }
+
+ function dividir() {
+     resultado.innerHTML = parseInt(n1.value) / (parseInt(n2.value));
+ }
+
+ function porcentagem() {
+
+ }
